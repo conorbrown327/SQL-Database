8e9cd4b09059d2ec137ebb23f0e9db8b0af6c19a
meye3181 <meye3181@umn.edu>
2021-12-05 17:41:35 -0600

    Update adult_pat.sql

--- Task3/adult_pat.sql
+++ Task3/adult_pat.sql
@@ -1,5 +1,5 @@
--- Creating table to store an adult patient's information and setting up foreign key relation
--- Author(s): Jacob Sandler
+-- Creating table to store an adult patient's information and setting up foreign key relations
+-- Author(s): Jacob Sandler, Zane Meyer
 CREATE TABLE ADULT_PAT (
   PatId BIGINT NOT NULL,
     PRIMARY KEY (PatId),
