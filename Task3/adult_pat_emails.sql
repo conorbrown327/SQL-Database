26c015ad88f54799a7b8142edeb66a18330bfe1e
meye3181 <meye3181@umn.edu>
2021-12-05 17:42:48 -0600

    Update adult_pat_emails.sql

--- Task3/adult_pat_emails.sql
+++ Task3/adult_pat_emails.sql
@@ -1,5 +1,5 @@
--- Creating table to store an adult patient's emails
--- Author(s): Jacob Sandler, Conor Brown
+-- Creating table to store an adult patient's emails and setting up foreign key relations
+-- Author(s): Jacob Sandler, Conor Brown, Zane Meyer
 CREATE TABLE ADULT_PAT_EMAILS (
 	PatId BIGINT NOT NULL,
     -- got error when using type text so changed to varchar 
