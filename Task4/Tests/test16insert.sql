900b33f439aa243526316ad01eae7a13070904fd
Zane Meyer <meye3181@umn.edu>
2021-12-15 16:05:59 -0600

    added Test 16-20

---
+++ Task4/Tests/test16insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO PATIENT
+VALUES (1238, "Zane", "R", "Meyer", "1600 test rd", '2011-01-01');
+INSERT INTO ADULT_PAT
+VALUES (1238);
+INSERT INTO EMPLOYEE
+VALUES (54, "Bill", "B", "Billson", 123456789);
+INSERT INTO NURSE
+VALUES (54, 54);
+INSERT INTO VISIT_RECORD
+VALUES (54, 1238, 17, "Sick", "Gone", 0.0, 0.0, "Under 9000", 67);/*same day*/
+INSERT INTO VISIT_RECORD
+VALUES (54, 1238, 19, "Sick", "Gone", 0.0, 0.0, "Under 9000", 67);/*same day*/
+INSERT INTO VISIT_RECORD
+VALUES (54, 1238, 27, "Sick", "Gone", 0.0, 0.0, "Under 9000", 67);/*different day*/
---
+++ Task4/Tests/test17query.sql
@@ -0,0 +1,3 @@
+SELECT Med_Condition, Pulse, Temp, Weight, Blood_Pressure, Temp
+FROM VISIT_RECORD
+WHERE PatId = 1238 AND VisitID = 2111
